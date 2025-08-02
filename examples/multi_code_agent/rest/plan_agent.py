42b464f0a3f4e60fc8bd7ac20d35665ec09e04fc
Zecheng Zhang <zecheng@traceroot.ai>
2025-08-01 18:47:46 -0700

    Add logger setup and debug statement in plan_query

---
+++ examples/multi_code_agent/rest/plan_agent.py
@@ -0,0 +1,12 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class PlanAgent:
+    def plan_query(self, query):
+        logger.debug(f"Planning query: {query}")
+        # Existing plan generation logic follows below
+        # TODO: implement or modify the logic as needed
+        plans = []
+        return plans
