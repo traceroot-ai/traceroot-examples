e58d65b6bfe4d2107a121d16c56ebd813f14cf04
Zecheng Zhang <zecheng@traceroot.ai>
2025-08-09 15:32:41 -0700

    chore: improve error log messages in example

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -9,14 +9,16 @@ logger = traceroot.get_logger()
 def logging_function_2():
     logger.info("This is an info message 2")
     logger.warning("This is a warning message 2")
-    logger.error("This is an error message 2")
+-    logger.error("This is an error message 2")
++    logger.error("An error occurred in logging_function_2")
 
 
 @traceroot.trace()
 def logging_function():
     logger.info("This is an info message")
     logger.warning("This is a warning message")
-    logger.error("This is an error message")
+-    logger.error("This is an error message")
++    logger.error("An error occurred in logging_function")
     logging_function_2()
 
 
