b25f7684d9f50d959d402691570a28e5c66def17
Zecheng Zhang <zecheng@traceroot.ai>
2025-08-09 16:15:26 -0700

    Include exc_info in error logs for stacktrace visibility

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -4,21 +4,20 @@ import traceroot
 
 logger = traceroot.get_logger()
 
-
 @traceroot.trace()
 def logging_function_2():
     logger.info("This is an info message 2")
     logger.warning("This is a warning message 2")
-    logger.error("This is an error message 2")
-
+    # Include exception info for better debugging
+    logger.error("This is an error message 2", exc_info=True)
 
 @traceroot.trace()
 def logging_function():
     logger.info("This is an info message")
     logger.warning("This is a warning message")
-    logger.error("This is an error message")
+    # Include exception info for better debugging
+    logger.error("This is an error message", exc_info=True)
     logging_function_2()
-
 
 @traceroot.trace()
 def main():
@@ -26,7 +25,6 @@ def main():
     time.sleep(1)
     logging_function()
     logger.debug("Main function completed")
-
 
 if __name__ == "__main__":
     main()
