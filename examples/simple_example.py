607540c81c2dcce70eda38c997a6c1c56319dcdf
Zecheng Zhang <zecheng@traceroot.ai>
2025-08-09 17:08:03 -0700

    Replace error logs with logger.exception to capture stack traces

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -4,19 +4,20 @@ import traceroot
 
 logger = traceroot.get_logger()
 
-
 @traceroot.trace()
 def logging_function_2():
     logger.info("This is an info message 2")
     logger.warning("This is a warning message 2")
-    logger.error("This is an error message 2")
+-    logger.error("This is an error message 2")
++    logger.exception("This is an error message 2")
 
 
 @traceroot.trace()
 def logging_function():
     logger.info("This is an info message")
     logger.warning("This is a warning message")
-    logger.error("This is an error message")
+-    logger.error("This is an error message")
++    logger.exception("This is an error message")
     logging_function_2()
 
 
